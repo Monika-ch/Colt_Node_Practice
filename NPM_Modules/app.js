4eabeca690b6c2f0ae5d7a87e1be95f75a8286a2
monika12b <monika12b@gmail.com>
2023-04-18 18:53:09 -0700

    Requiring a directory

--- NPM_Modules/app.js
+++ NPM_Modules/app.js
@@ -1,10 +1,13 @@
 const math = require("./math");
-console.log(math);
-console.log(math.PI);
-console.log(math.square(9));
+// console.log(math);
+// console.log(math.PI);
+// console.log(math.square(9));
 
 // // OR-----
 
 const { PI, square } = require("./math");
-console.log(PI);
-console.log(square(4));
+// console.log(PI);
+// console.log(square(4));
+
+const cats = require("./shelter");
+console.log("REQUIRED AN ENTIRE DIRECTORY", cats);
