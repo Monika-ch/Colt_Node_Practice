e8b0ac6bed076a4434b8c95411c3ac9642bba512
monika12b <monika12b@gmail.com>
2023-05-01 17:49:55 -0700

    request and response objects

--- Express_Intro/firstApp/index.js
+++ Express_Intro/firstApp/index.js
@@ -2,7 +2,19 @@ const express = require("express");
 const app = express();
 console.dir(app);
 
-app.use(() => console.log("WE GOT A NEW REQUEST"));
+// app.use(() => console.log("WE GOT A NEW REQUEST"));
+
+// // In the above line of code of "app.use", Express creates 2 objects for us- (incoming) "request" and (outgoing) "response".
+// // Express parses the incoming HTTP request information and then passes it in the first argument as "request" to this callback
+// // So, we have access to 2 different params (made by Express). in this function that is automatically passed in the callback
+
+app.use((req, res) => {
+  console.log("WE GOT A NEW REQUEST");
+  // console.dir(req);
+  // res.send("HELLO, WE GO YOUR REQUEST! THIS IS THE RESPONSE!") // // sending string in response
+  // res.send({ color: "red" }); // // sending object in response
+  res.send("<h1>This is my webpage!</h1>"); // // sending html in response
+});
 
 // // Ports: 8080, 3000
 const port = 8080;
