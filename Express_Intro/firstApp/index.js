363b90681942592e41b8ef89c2f3c45d0c0d41b6
monika12b <monika12b@gmail.com>
2023-05-02 23:16:04 -0700

    Express Routing Basics

--- Express_Intro/firstApp/index.js
+++ Express_Intro/firstApp/index.js
@@ -8,12 +8,38 @@ console.dir(app);
 // // Express parses the incoming HTTP request information and then passes it in the first argument as "request" to this callback
 // // So, we have access to 2 different params (made by Express). in this function that is automatically passed in the callback
 
-app.use((req, res) => {
-  console.log("WE GOT A NEW REQUEST");
-  // console.dir(req);
-  // res.send("HELLO, WE GO YOUR REQUEST! THIS IS THE RESPONSE!") // // sending string in response
-  // res.send({ color: "red" }); // // sending object in response
-  res.send("<h1>This is my webpage!</h1>"); // // sending html in response
+// // ****SENDING REQUEST/RESPONSE****
+
+// app.use((req, res) => {
+//   console.log("WE GOT A NEW REQUEST");
+//   // console.dir(req);
+//   // res.send("HELLO, WE GO YOUR REQUEST! THIS IS THE RESPONSE!") // // sending string in response
+//   // res.send({ color: "red" }); // // sending object in response
+//   res.send("<h1>This is my webpage!</h1>"); // // sending html in response
+// });
+
+// // ****ROUTING****
+
+app.get("/", (req, res) => {
+  //.get takes 2 params- path and a callback function
+  res.send("<h1>This is the home page!</h1>");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("MEOWW!!");
+});
+
+app.post("/cats", (req, res) => {
+  res.send("POST REQUEST TO /cats!!!! THIS IS DIFFERENT THAN A GET REQUEST!");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("WOOFF!!");
+});
+
+app.get("*", (req, res) => {
+  // "*" is supposed to be at very end! It means to match everything else!
+  res.send("I don't know that path!!");
 });
 
 // // Ports: 8080, 3000
